bf23b282229c0aab74470382f7e79783e7c6958c
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-16 20:47:11 -0300

    feat: cria CarModel.

---
+++ src/Models/Car.model.ts
@@ -0,0 +1,26 @@
+import { Model, Schema, model, models } from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+class CarModel {
+  private schema: Schema;
+  private model: Model<ICar>;
+
+  constructor() {
+    this.schema = new Schema<ICar>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: true },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+    this.model = models.Car || model('Car', this.schema);
+  }
+
+  public async create(car: ICar): Promise<ICar> {
+    return this.model.create({ ...car });
+  }
+}
+
+export default CarModel;
