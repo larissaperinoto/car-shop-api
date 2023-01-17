05d80e9e68a7c51d426610cccbc6a4ab8fc4cc9d
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-17 20:01:25 -0300

    tests: insere teste para a função updateById

--- tests/unit/Services/cars.test.ts
+++ tests/unit/Services/cars.test.ts
@@ -14,32 +14,34 @@ const carReq: ICar = {
   seatsQty: 5,
 };
 
+const id = '6348513f34c397abcad040b2';
+
 describe('Testa a camada service para a rota /cars', function () {
   it('Testa o método POST com a função "create"', async function () {
-    sinon.stub(Model, 'create').resolves({ id: '6348513f34c397abcad040b2', ...carReq });
+    sinon.stub(Model, 'create').resolves({ id, ...carReq });
 
     const service = new CarService();
     const result = await service.create(carReq);
 
-    expect(result).to.be.deep.equal({ id: '6348513f34c397abcad040b2', ...carReq });
+    expect(result).to.be.deep.equal({ id, ...carReq });
   });
 
   it('Testa o método GET com a função "findAll"', async function () {
-    sinon.stub(Model, 'find').resolves([{ id: '6348513f34c397abcad040b2', ...carReq }]);
+    sinon.stub(Model, 'find').resolves([{ id, ...carReq }]);
 
     const service = new CarService();
     const result = await service.findAll();
 
-    expect(result).to.be.deep.equal([{ id: '6348513f34c397abcad040b2', ...carReq }]);
+    expect(result).to.be.deep.equal([{ id, ...carReq }]);
   });
 
   it('Testa o método GET com a função "findById" quando o id existe', async function () {
-    sinon.stub(Model, 'findOne').resolves({ id: '6348513f34c397abcad040b2', ...carReq });
+    sinon.stub(Model, 'findOne').resolves({ id, ...carReq });
 
     const service = new CarService();
-    const result = await service.findById('6348513f34c397abcad040b2');
+    const result = await service.findById(id);
 
-    const response = { id: '6348513f34c397abcad040b2', ...carReq };
+    const response = { id, ...carReq };
 
     expect(result).to.be.deep.equal({ status: 200, response });
   });
@@ -57,11 +59,21 @@ describe('Testa a camada service para a rota /cars', function () {
     sinon.stub(Model, 'findOne').resolves(null);
 
     const service = new CarService();
-    const result = await service.findById('6348513f34c397abcad040b2');
+    const result = await service.findById(id);
 
     const message = { message: 'Car not found' };
 
     expect(result).to.be.deep.equal({ status: 404, response: message });
+  });
+
+  it('Testa o método PUT com a função "updateById" quando o carro existe', async function () {
+    sinon.stub(Model, 'findOne').resolves({ id, ...carReq });
+    sinon.stub(Model, 'updateOne').resolves();
+
+    const service = new CarService();
+    const result = await service.updateById(id, carReq);
+
+    expect(result).to.be.deep.equal({ status: 200, response: { id, ...carReq } });
   });
 
   afterEach(function () {
