d93091da72eae5054b75d8fc8ee86d88f50789a5
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-18 17:50:19 -0300

    test: insere testes de unidade da camada service

--- tests/unit/Services/motorcycle.test.ts
+++ tests/unit/Services/motorcycle.test.ts
@@ -25,4 +25,31 @@ describe('Testa a camada service para a rota /motorcycles', function () {
 
     expect(result).to.be.deep.equal({ id, ...motorcycleReq });
   });
+
+  it('Testa o método GET com a função "findAll"', async function () {
+    sinon.stub(Model, 'find').resolves([{ id, ...motorcycleReq }]);
+
+    const service = new MotorcycleService();
+    const result = await service.findAll();
+
+    expect(result).to.be.deep.equal([{ id, ...motorcycleReq }]);
+  });
+
+  it('Testa o método GET com a função "findById"', async function () {
+    sinon.stub(Model, 'findOne').resolves({ id, ...motorcycleReq });
+
+    const service = new MotorcycleService();
+    const result = await service.findById(id);
+
+    expect(result).to.be.deep.equal({ status: 200, response: { id, ...motorcycleReq } });
+  });
+
+  it('Testa o método GET com a função "updateById"', async function () {
+    sinon.stub(Model, 'updateOne').resolves();
+
+    const service = new MotorcycleService();
+    const result = await service.updateById(id, motorcycleReq);
+
+    expect(result).to.be.deep.equal({ status: 200, response: { id, ...motorcycleReq } });
+  });
 });
